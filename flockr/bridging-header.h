ee7376caa4f4bcd78701a0600a9370d5b885c370
Matheus Alberton <matheusfrozzi@gmail.com>
2016-11-27 05:02:53 -0200

    fix compilar

---
+++ flockr/bridging-header.h
@@ -0,0 +1,13 @@
+//
+//  bridging-header.h
+//  flockr
+//
+//  Created by Matheus Frozzi Alberton on 17/07/15.
+//  Copyright (c) 2015 flockr. All rights reserved.
+//
+
+#ifndef flockr_bridging_header_h
+#define flockr_bridging_header_h
+
+#import <Google/Analytics.h>
+#endif
